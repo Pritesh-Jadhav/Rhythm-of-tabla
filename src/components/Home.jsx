d844bc24de741630eea4928b7df920c257f78867
Pritesh-Jadhav <priteshjadhav400@gmail.com>
2026-03-16 11:28:07 +0530

    update

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,7 +5,7 @@ import "./Home.css";
 const SLIDES = [
   {
     id: 1,
-    tag: "Hello",
+    tag: "PERCUSSION",
     badge: "🥁 The Tabla",
     heading: "Soul of Indian",
     headingAccent: "Classical Music",
